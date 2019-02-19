2bbf8ed7312c756c89fce808b1ce07807c8ea9d6
Darwon Rashid <darwon97@gmail.com>
2019-02-19 19:20:18 +0000

    testing out drawing board on console

--- TicTacToe.c
+++ TicTacToe.c
@@ -1,13 +1,68 @@
+/* Author: Darwon Rashid
+Matric Number - 40280334
+*/
+
+// Libraries to be included for code.
 #include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+#define HORIZONTAL 3
+#define VERTICAL 3
+
 
 //path
 // C:\Users\darwo\Desktop\All Workspace\Workspace\C and C++ Workspace\Data Structures and Algortihms\Coursework
 
 
 
+void renderGameBoard()
+{
+
+}
+
+void renderGameBoard();
+
 int main(int argc, char ** argv)
 {
-    
+    char *state = "---111000";
+
+
+    int length = strlen(state);
+
+    int count = 0;
+
+    for(int i = 0; i < length; i++)
+    {   
+
+        printf("%c", state[i]);
+        count++;
+/*
+        if( i % 3 == 0)
+        {
+            printf("\n");
+        }
+
+        if(state[i] == '-')
+        {
+            printf("%c", '_');
+        }
+        else if(state[i] == '1')
+        {
+            printf("%c", 'X');
+        }
+        else if(state[i] == '0')
+        {
+            printf("%c", 'O');
+        }
+        
+        */
+
+      
+
+     
+    }
+
+    printf("%d", count);
 }
